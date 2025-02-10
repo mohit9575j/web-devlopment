e83f3cd544ee537e303eac1ee92fc2c109ed7110
mohit <joshimohit8120@gmail.com>
2025-02-10 23:43:36 +0530

    Updated list styles: Added yellow background to 3rd item & made all items bold

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-// Write your code below:
-const mainHeading = document.getElementById("main-heading");
+ const mainHeading = document.getElementById("main-heading");
 mainHeading.textContent = "Fruit World";
 
-// 2. Change the font color of 'Fruit World' from black to orange
-mainHeading.style.color = "orange";
+ mainHeading.style.color = "orange";
 
-// 3. Change background color of div with id = "header" to green color
-const headerDiv = document.getElementById("header");
+ const headerDiv = document.getElementById("header");
 headerDiv.style.backgroundColor = "green";
 
-// 4. Add a bottom border of orange color to div with id = "header"
-headerDiv.style.borderBottom = "2px solid orange";
+ headerDiv.style.borderBottom = "2px solid orange";
 
-// 5. Change the font color of 'Fruits In Basket' from black to green
-const basketHeading = document.getElementById("basket-heading");
+ const basketHeading = document.getElementById("basket-heading");
 basketHeading.style.color = "green";
 
-// 6. Introduce a paragraph element with text "Please visit us again" inside the div with id = "thanks"
 const thanksDiv = document.getElementById("thanks");
 const newParagraph = document.createElement("p");
 newParagraph.textContent = "Please visit us again";
 thanksDiv.appendChild(newParagraph);
+
+const fruitItems = document.querySelectorAll(".fruit");
+
+fruitItems[2].style.backgroundColor = "yellow";
+
+fruitItems.forEach((fruit) => {
+    fruit.style.fontWeight = "bold";
+});
